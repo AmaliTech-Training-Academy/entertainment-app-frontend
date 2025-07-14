764bfdeb1ea7cec448be584f8b97c88aa0af96b3
iamernesto14 <eernesto211@gmail.com>
2025-07-14 14:29:36 +0000

    feat(sign-up): implement reactive form with validation for registration

--- src/features/auth/sign-up/sign-up.component.ts
+++ src/features/auth/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Form Data:', this.registerForm.value);
+      // API call will be added here later
+    } else {
+      console.log('Form Invalid');
+      this.registerForm.markAllAsTouched();
+    }
+  }
 }
