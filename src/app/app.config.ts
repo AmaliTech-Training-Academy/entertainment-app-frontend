17b451bbaac21ce16be8268adf2e3137724cfdce
ewurasi nyarkoa <anewurasi@gmail.com>
2025-07-14 21:27:12 +0000

    created a shared sign-in button component

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { CORE_PROVIDERS } from './core/core.module';
-// import { SHARED_IMPORTS } from './shared/shared.module';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    ...CORE_PROVIDERS
-    // ...SHARED_IMPORTS (for standalone shared imports if needed)
+    provideRouter(routes)
   ]
 };
