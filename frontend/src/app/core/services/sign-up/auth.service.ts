b9d52304556748258d6c3fa0365912d243da56bb
Clement <clementnyankuadjei@gmail.com>
2025-07-17 20:03:40 +0000

    Refactor navbar and routing; update API endpoint and add CSP
    - Added a new route for the user dashboard and linked it in the navbar.
    - Updated the API endpoint in the AuthService for user registration.
    - Introduced a Content Security Policy meta tag in index.html for enhanced security.
    - Refactored imports in Angular Material dependencies and added styles for the new navbar link.
    - Removed unused comment and error handler service files to clean up the codebase.

--- frontend/src/app/core/services/sign-up/auth.service.ts
+++ frontend/src/app/core/services/sign-up/auth.service.ts
@@ -22,7 +22,7 @@ export class AuthService {
 
   register(payload: RegisterPayload): Observable<any> {
     console.log(payload)
-    return this.http.post(`http://cineverse-service-alb-staging-346786246.eu-west-1.elb.amazonaws.com/api/v1/auth/register`, payload);
+    return this.http.post(`http://cineverse-service-alb-staging-276074081.eu-west-1.elb.amazonaws.com/api/v1/auth/register`, payload);
   }
 
   // You can add other API methods here (login, etc.)
