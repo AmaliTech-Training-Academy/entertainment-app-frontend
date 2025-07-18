03be859406fb81ea1635350640c55b696258c833
Yesukay <yesukay2@gmail.com>
2025-07-18 09:46:04 -0400

    orrect mock api base url

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiBaseUrl: 'https://jsonfakery.com/movies/random/',
+  apiBaseUrl: 'https://jsonfakery.com/movies/random',
   apiUrl:
     'http://cineverse-service-alb-staging-276074081.eu-west-1.elb.amazonaws.com',
 };
