405d13de101083f66b4f81922ae8e0e92801b686
iamernesto14 <eernesto211@gmail.com>
2025-07-18 13:38:20 +0000

    Fix: route to home issue

--- frontend/src/app/features/auth/sign-up/sign-up.component.ts
+++ frontend/src/app/features/auth/sign-up/sign-up.component.ts
@@ -151,7 +151,7 @@ export class SignUpComponent {
           }
           // Optionally store user info in cookies as well if needed
           // Redirect to home page
-          this.router.navigate(['/user']);
+          this.router.navigate(['/login']);
         },
         error: (err) => {
           this.loading = false;
