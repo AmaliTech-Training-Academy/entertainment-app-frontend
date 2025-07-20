9d888bfc5ceac007329547f27d25afa09740a074
ewurasi nyarkoa <anewurasi@gmail.com>
2025-07-20 23:51:33 +0000

    uncommented the metrix which was causing my build to fail

--- frontend/src/app/components/dashboard-metrics/dashboard-metrics.component.ts
+++ frontend/src/app/components/dashboard-metrics/dashboard-metrics.component.ts
@@ -1,21 +1,11 @@
-// import { Component, OnInit } from '@angular/core';
-// import { AdminService } from '../../core/services/admin/admin.service';
+import { Component } from '@angular/core';
 
-// @Component({
-//   selector: 'app-dashboard-metrics',
-//   templateUrl: './dashboard-metrics.component.html',
-//   styleUrls: ['./dashboard-metrics.component.scss'],
-// })
-// export class DashboardMetricsComponent implements OnInit {
-//   userCount = 0;
-//   mediaCount = 0;
-
-//   constructor(private adminService: AdminService) {}
-
-//   ngOnInit(): void {
-//     this.adminService.getMetrics().subscribe((res) => {
-//       this.userCount = res.data.userCount;
-//       this.mediaCount = res.data.mediaCount;
-//     });
-//   }
-// }
+@Component({
+  selector: 'app-dashboard-metrics',
+  templateUrl: './dashboard-metrics.component.html',
+  styleUrls: ['./dashboard-metrics.component.scss'],
+})
+export class DashboardMetricsComponent {
+  userCount = 0;
+  mediaCount = 0;
+}
