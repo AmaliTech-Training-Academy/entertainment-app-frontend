5ef892244def50d154fea975ad37a4c045b8b5ce
Yesukay <yesukay2@gmail.com>
2025-07-20 23:33:07 -0400

    Add form for admin media submission

---
+++ frontend/src/app/core/services/add-media/add-media.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AddMediaService {
+  private apiUrl = `${environment.apiBaseUrl}/media`;
+
+  constructor(private http: HttpClient) {}
+
+  createMedia(mediaData: FormData): Observable<any> {
+    return this.http.post(this.apiUrl, mediaData);
+  }
+}
