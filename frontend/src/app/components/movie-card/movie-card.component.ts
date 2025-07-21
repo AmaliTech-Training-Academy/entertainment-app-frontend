92c9b7bc2b13fa997eb8de4de517d4ee0a1ce3fe
ewurasi-nyarkoa <anewurasi@gmail.com>
2025-07-21 09:39:31 +0000

    Update movie-card.component.ts

--- frontend/src/app/components/movie-card/movie-card.component.ts
+++ frontend/src/app/components/movie-card/movie-card.component.ts
@@ -2,9 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { Movie } from '../../core/services/advance-search/advanced-search.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
+  standalone: true,
   imports: [MatIcon, CommonModule],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.scss',
@@ -13,6 +15,11 @@ export class MovieCardComponent {
 
   constructor(private router: Router) {}
   // @Input() movie!: Movie;
+   navigateToDetails(mediaId: number) {
+    if (mediaId) {
+      this.router.navigate(['/media', mediaId]);
+    }
+  }
   @Input() movie!: any;
 
   toggleBookmark() {
