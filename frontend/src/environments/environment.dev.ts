8998ddd74f8a04db797db8dc5d69929bcfecdbfc
Yesukay <yesukay2@gmail.com>
2025-07-20 21:06:52 -0400

    Fix: restructure environement setup

--- frontend/src/environments/environment.dev.ts
+++ frontend/src/environments/environment.dev.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: false,
-  // apiUrl: process.env['NG_APP_API_URL'],
   apiBaseUrl:
     'http://cineverse-service-alb-staging-276074081.eu-west-1.elb.amazonaws.com/api/v1/media',
 };
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: process.env['NG_APP_URL'],
   apiBaseUrl:
     'http://cineverse-service-alb-staging-276074081.eu-west-1.elb.amazonaws.com/api/v1/media',
 };
